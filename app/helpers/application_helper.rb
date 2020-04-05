9be1e68ba179414634a75260fb2bd0eea0b09314
yuan <qmyemail@gmail.com>
2020-04-05 17:26:45 +0800

    fix converting ms to text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def ms_to_str(ms)
+    remainder = ms
+    r = []
+    [3600000, 60000, 1000].each do |base|
+      quotient = remainder.fdiv(base).round
+      remainder = remainder % base
+      r << quotient
+    end
+    r.join(':') << ".#{remainder}"
+  end
 end
