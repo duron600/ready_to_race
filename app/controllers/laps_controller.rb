372752a0a7dbca4bbbf708dbbc55f772e53e5b93
yuan <qmyemail@gmail.com>
2020-04-06 19:51:16 +0800

    laps order by created_at

--- app/controllers/laps_controller.rb
+++ app/controllers/laps_controller.rb
@@ -1,6 +1,6 @@
 class LapsController < ApplicationController
   def index
     @racing_session = RacingSession.find params[:racing_session_id]
-    @laps = @racing_session.laps
+    @laps = @racing_session.laps.order(:created_at)
   end
 end
