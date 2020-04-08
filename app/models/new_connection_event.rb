4fac042432bba8da5997659a96546d7bb61b9bd5
yuan <qmyemail@gmail.com>
2020-04-08 20:56:17 +0800

    fix closing old connection while new connection creating

--- app/models/new_connection_event.rb
+++ app/models/new_connection_event.rb
@@ -5,7 +5,7 @@ class NewConnectionEvent < AcServerEvent
   private
 
   def save_parsed_content
-    ClientConnection.alive.where(:car_id => car.id).each(&:close)
+    ClientConnection.alive.where(:car_index => car_index).each(&:close)
     ClientConnection.create({
       :driver => driver,
       :car => car,
